09ccddb25df31d05f8b5a00239085da2990c99d6
jgzhang3397 <jgzhang1998@gmail.com>
2022-05-12 17:16:19 +0200

    add

--- tutorial/demo18_get_input_text.asm
+++ tutorial/demo18_get_input_text.asm
@@ -1,8 +1,30 @@
-### getting users inputs Text
+# getting users inputs Text
 
 .data
-	
+	message: 	.asciiz "Hello,"
+	userInput: 	.space 20 # 20 characters
 .text
+	main:
+		# getting users input as text
+		li $v0, 8 # 8 is the code, tell the system to read inputs text
+		la $a0, userInput # store the users input
+		la $a1, 20 # the maximum length of the text
+		syscall
+		
+		# Display hello
+		li $v0, 4
+		la $a0, message
+		syscall
+		
+		# Display the usersname
+		li $v0, 4
+		la $a0, userInput
+		syscall
+		
+	
+	# the end of main
+	li $v0, 10
+	syscall
 	
 	
 
