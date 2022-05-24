6e38edb51ba5c1bcf541e8620a26827a4511337f
jgzhang3397 <jgzhang1998@gmail.com>
2022-05-24 18:18:16 +0200

    modify

--- tutorial/demo11_divisionInteger_sll.asm
+++ tutorial/demo11_divisionInteger_sll.asm
@@ -1,24 +1,34 @@
-### dividing Integer
+# dividing Integer
 
 .data
-
+	newline: .asciiz "\n"
 .text
 	addi $s0, $zero, 30
 	addi $s1, $zero, 8
 	
-	#method 1
-	#div  $t0, $s0, $s1
+	# method 1
+	# div $t0, $s0, $s1
 	
-	#method 2
-	#div $t0, $s0, 2
+	# method 2
+	# div $t0, $s0, 2
 	
-	#method 3 
+	# method 3 
 	div $s0, $s1
 	mflo $t0 # Quatient
 	mfhi $t1 # Remainder
 	
-	#print it
-	li $v0, 1
+	# print Quatient
+	li  $v0, 1
+	add $a0, $zero, $t0
+	syscall
+	
+	# print newline
+	li  $v0, 4
+	la  $a0, newline
+	syscall
+	
+	# print Remainder
+	li  $v0, 1
 	add $a0, $zero, $t1
 	syscall
 	
