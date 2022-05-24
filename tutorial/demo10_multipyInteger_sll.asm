92b0eb8021e46a90d8814bc926af2d30306955df
jgzhang3397 <jgzhang1998@gmail.com>
2022-05-24 18:14:03 +0200

    modifiy

--- tutorial/demo10_multipyInteger_sll.asm
+++ tutorial/demo10_multipyInteger_sll.asm
@@ -3,14 +3,14 @@
 .data
 
 .text
-	addi $s0, $zero, 4
+	addi $s0, $zero, 5
 	
 	# shift left logical
-	sll $t0, $s0, 2
+	sll $t0, $s0, 3 # $s0 * 2^3
 	
 	# print it
 	li $v0, 1
-	add $a0, $zero, $t0
+	add $a0, $zero, $t0 # 40
 	syscall
 	
 	
